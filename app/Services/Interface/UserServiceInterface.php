dabd5f3951cb82da5f3f08c1a7b62d4f622ac525
BraianRico <daviidriico@gmail.com>
2022-01-06 15:09:32 -0500

    UserServiceInterface

---
+++ app/Services/Interface/UserServiceInterface.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Services\Interface;
+
+interface UserServiceInterface
+{
+
+    /**
+     *
+     * @return void
+     */
+    function listUser();
+
+    /**
+     *
+     * @param integer $id
+     * @return void
+     */
+    function SearchUser(int $id);
+
+    /**
+     *
+     * @param array $user
+     * @return void
+     */
+    function createUser(array $user);
+
+    /**
+     *
+     * @param array $user
+     * @param integer $id
+     * @return void
+     */
+    function updateUser(array $user, int $id);
+
+    /**
+     *
+     * @param integer $id
+     * @return boolean
+     */
+    function deleteUser(int $id);
+
+    /**
+     * 
+     * @param integer $id
+     * @return boolean
+     */
+    function restoreUser(int $id);
+}
