6cb941a2f8ed1c70e58d469ff9dc290be71426f6
Braian1 <david.rico@iudigital.edu.co>
2022-02-19 22:27:26 -0500

    Prueba 1

---
+++ app/Http/Controllers/Braian1.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller as ControllersController;
+
+class Controller extends ControllersController
+{
+    public function braian1Create()
+    {
+    }
+}
